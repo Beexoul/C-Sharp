e3900bfafe91071ea71de07574b35be587abf24b
Shiva Raj Paudel <shivarajpaudel.contact@gmail.com>
2023-07-22 17:05:33 +0545

    Arithmetic Operations.cs

---
+++ Arithmetic Operations.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Difference: " + difference);
+        Console.WriteLine("Product: " + product);
+        Console.WriteLine("Quotient: " + quotient);
+        Console.WriteLine("Remainder: " + remainder);
+    }
+}
