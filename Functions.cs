121f60b5d6b012f86cf376f9ae71c7a65dbc59dd
Shiva Raj Paudel <shivarajpaudel.contact@gmail.com>
2023-07-22 17:07:45 +0545

    Functions.cs

---
+++ Functions.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int result = Add(3, 5);
+        Console.WriteLine("The sum is: " + result);
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
