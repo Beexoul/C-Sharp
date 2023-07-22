2d75ca511b20cb42fb58faddf68bed76b8ecbcda
Shiva Raj Paudel <shivarajpaudel.contact@gmail.com>
2023-07-22 17:06:15 +0545

    Conditional Statements.cs

---
+++ Conditional Statements.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are an adult.");
+        }
+        else
+        {
+            Console.WriteLine("You are a minor.");
+        }
+    }
+}
