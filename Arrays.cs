12a01c3fa217f07a82468d64321be8c83e50fda0
Shiva Raj Paudel <shivarajpaudel.contact@gmail.com>
2023-07-22 17:07:15 +0545

    Arrays.cs

---
+++ Arrays.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
