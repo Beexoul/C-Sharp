4c7d1bb36532633fd4f5b0c767f766d14b4e63dd
Shiva Raj Paudel <shivarajpaudel.contact@gmail.com>
2023-07-22 17:04:16 +0545

    Create Input And Output.cs

---
+++ Input And Output.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
